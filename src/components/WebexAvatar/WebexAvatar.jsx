0bce00ca141cc8ae141a20e3575dacbe2d7de58a
Victor Malai <malai9696@gmail.com>
2021-04-26 13:13:00 +0300

    feat(config): add dependendabot and workflow

--- src/components/WebexAvatar/WebexAvatar.jsx
+++ src/components/WebexAvatar/WebexAvatar.jsx
@@ -15,7 +15,7 @@ import {usePerson} from '../hooks';
 export default function WebexAvatar({personID, displayStatus}) {
   const {avatar, displayName, status} = usePerson(personID);
   const type = displayStatus ? status : null;
-
+  alert(123);
   return <Avatar src={avatar} title={displayName} type={type} alt={displayName} />;
 }
 
